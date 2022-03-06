01002b2e9336707220b952872960eacd55345563
vishal00923 <vishalbg.mz@gmail.com>
2022-03-06 17:21:04 +0530

    Update Root

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
+import 'react-alice-carousel/lib/alice-carousel.css';
+
 import App from './App';
 
 import './index.css';
