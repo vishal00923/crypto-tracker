075ddb5e934786b2791573462523c86f3e67c55e
vishal00923 <vishalbg.mz@gmail.com>
2022-03-09 01:58:09 +0530

    Add HomePage CoinPage util

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import Banner from '../Components/Banner/Banner';
+import CoinsTable from '../Components/CoinsTable/CoinsTable';
+
+const HomePage = ({ currency, symbol }) => {
+    return (
+        <>
+            <Banner
+                currency={currency}
+                symbol={symbol}
+            />
+            <CoinsTable
+                currency={currency}
+                symbol={symbol}
+            />
+        </>
+    );
+};
+
+export default HomePage;
---
+++ src/util.js
@@ -0,0 +1,4 @@
+// Large Number Formatter
+export const numberWithCommas = (x) => {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+};
