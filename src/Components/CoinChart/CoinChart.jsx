56eec4e42ca1817ed4310eee5a77c9fe606ae8f3
vishal00923 <vishalbg.mz@gmail.com>
2022-03-10 23:54:34 +0530

    Add CoinChart Component

---
+++ src/Components/CoinChart/CoinChart.jsx
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { CircularProgress, ThemeProvider } from '@material-ui/core';
+import { Line } from 'react-chartjs-2';
+
+import { useStyles, darkTheme } from './styles';
+
+const CoinChart = ({ coin, currency }) => {
+  const [days, setDays] = useState(1);
+  const [historicalData, setHistoricalData] = useState([]);
+
+  const id = coin.id;
+  const classes = useStyles();
+
+  useEffect(() => {
+    // Fetch Historical Chart Data
+    const url = `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=${currency}&days=${days}`;
+
+    const fetchHistoricalChart = async () => {
+      const { data } = await axios.get(url);
+
+      setHistoricalData(data.prices);
+    };
+
+    fetchHistoricalChart();
+  }, [id, currency, days]);
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <div className={classes.container}>
+        {!historicalData ? (
+          <CircularProgress className={classes.circularProgress} />
+        ) : (
+          <>
+            <Line
+              data={{
+                labels: historicalData.map((coin) => {
+                  let date = new Date(coin[0]);
+                  let hours = date.getHours();
+                  let minutes = date.getMinutes();
+                  let time =
+                    hours > 12
+                      ? `${hours - 12}:${minutes} PM`
+                      : `${hours}:${minutes} AM`;
+
+                  return days === 1 ? time : date.toLocaleDateString();
+                }),
+
+                datasets: [
+                  {
+                    data: historicalData.map((coin) => coin[1]),
+                    label: `Price (Past ${days} Days) in ${currency}`,
+                    borderColor: '#eebc1d',
+                  },
+                ],
+              }}
+              options={{
+                elements: {
+                  point: {
+                    radius: 1,
+                  },
+                },
+              }}
+            />
+          </>
+        )}
+      </div>
+    </ThemeProvider>
+  );
+};
+
+export default CoinChart;
---
+++ src/Components/CoinChart/styles.jsx
@@ -0,0 +1,33 @@
+import { makeStyles, createTheme } from '@material-ui/core';
+
+export const useStyles = makeStyles((theme) => ({
+  container: {
+    width: '75%',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: '1.825rem',
+    padding: '2.825rem',
+    [theme.breakpoints.down('md')]: {
+      width: '100%',
+      marginTop: 0,
+      padding: '1.475rem',
+      paddingTop: 0,
+    },
+  },
+  circularProgress: {
+    size: 250,
+    thickness: 1,
+    color: '#ffe900',
+  },
+}));
+
+export const darkTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#fff',
+    },
+    type: 'dark',
+  },
+});
