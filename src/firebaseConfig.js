9acbe587ee7e19ee9e6da08a49e0240a092c55f3
Vishal Chaurasia <vishalbg.mz@gmail.com>
2022-05-18 16:46:49 +0530

    Update firebaseConfig

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-  apiKey: `${process.env.REACT_APP_API_KEY}`,
-  authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
-  projectId: `${process.env.REACT_APP_PROJECT_ID}`,
-  storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET}`,
-  messagingSenderId: `${process.env.REACT_APP_MESSAGING_SENDER_ID}`,
-  appId: `${process.env.REACT_APP_APP_ID}`,
-  measurementId: `${process.env.REACT_APP_MEASUREMENT_ID}`,
+  apiKey: 'AIzaSyB-QtLHMGNG_ZFNltA46paUO3OhCc42_Ic',
+  authDomain: 'crypto-tracker-4dbf0.firebaseapp.com',
+  projectId: 'crypto-tracker-4dbf0',
+  storageBucket: 'crypto-tracker-4dbf0.appspot.com',
+  messagingSenderId: '66820886492',
+  appId: '1:66820886492:web:e51f2225b2b4a5b858c919',
+  measurementId: 'G-VFW3EGLXVK',
 };
 
 export default firebaseConfig;
