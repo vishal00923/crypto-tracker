d8c8f073d3c336b49a63dcd98136c6ff49750ce6
Vishal Chaurasia <vishalbg.mz@gmail.com>
2022-09-08 11:18:24 +0530

    Update util

--- src/util.js
+++ src/util.js
@@ -1,10 +1,22 @@
+// api calls
+const coinsList = (currency) =>
+  `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=100&page=1&sparkline=false`;
+
+const singleCoin = (id) => `https://api.coingecko.com/api/v3/coins/${id}`;
+
+const historicalCharData = (id, days = 365, currency) =>
+  `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=${currency}&days=${days}`;
+
+const trendingCoinsData = (currency) =>
+  `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=gecko_desc&per_page=10&page=1&sparkline=false&price_change_percentage=24h`;
+
 // Large Number Formatter
-export const numberWithCommas = (x) => {
+const numberWithCommas = (x) => {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 };
 
 // Chart Data According To The No. Of Days
-export const chartData = [
+const chartData = [
   {
     label: '24 Hours',
     value: 1,
@@ -22,3 +34,12 @@ export const chartData = [
     value: 365,
   },
 ];
+
+export {
+  coinsList,
+  singleCoin,
+  historicalCharData,
+  trendingCoinsData,
+  numberWithCommas,
+  chartData,
+};
