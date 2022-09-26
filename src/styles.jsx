3d08c2b5416798864ecd6a1fa431aa04abcbaa3d
Vishal Chaurasia <vishalbg.mz@gmail.com>
2022-09-26 17:46:34 +0530

    remove files

--- src/styles.jsx
+++
@@ -1,9 +0,0 @@
-import { makeStyles } from '@material-ui/core';
-
-export default makeStyles(() => ({
-  app: {
-    backgroundColor: '#14161a',
-    color: '#fff',
-    minHeight: '100vh',
-  },
-}));
