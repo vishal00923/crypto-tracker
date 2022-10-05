4a62953100963bf16294f9d457a97df7e247db8a
Vishal Chaurasia <vishalbg.mz@gmail.com>
2022-10-05 18:56:19 +0530

    update components

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,14 +1,76 @@
+import { useContext, useState } from 'react';
+import { UserContext } from '../../contexts/userContext';
+import { signInUser } from '../../utils/firebase';
+
 import { Button, TextField } from '@mui/material';
 import { Box } from '@mui/system';
 
 import { sxStyles } from './Login.styles';
 
+const defaultFields = {
+  email: '',
+  password: '',
+};
+
 export default function Login() {
+  const [fields, setFields] = useState(defaultFields);
+  const { email, password } = fields;
+
+  const { setNotifications, setCurrentUser } = useContext(UserContext);
+
+  const handleSubmit = async () => {
+    if (!email || !password) {
+      setNotifications({
+        open: true,
+        type: 'error',
+        message: 'Please fill in all fields',
+      });
+      return;
+    }
+
+    try {
+      const { user } = await signInUser(email, password);
+
+      setNotifications({
+        open: true,
+        type: 'success',
+        message: 'Welcome back! ' + (user.displayName || user.email),
+      });
+      setCurrentUser(user);
+      setFields(defaultFields);
+    } catch (error) {
+      setNotifications({
+        open: true,
+        type: 'error',
+        message: error.message,
+      });
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFields({ ...fields, [name]: value });
+  };
+
   return (
     <Box sx={sxStyles.container}>
-      <TextField label="Enter Email" type="email" />
-      <TextField label="Enter Password" type="password" />
-      <Button sx={sxStyles.button}>login</Button>
+      <TextField
+        onChange={handleChange}
+        label="Email"
+        type="email"
+        name="email"
+        value={email}
+      />
+      <TextField
+        onChange={handleChange}
+        label="Password"
+        type="password"
+        name="password"
+        value={password}
+      />
+      <Button onClick={handleSubmit} sx={sxStyles.button}>
+        login
+      </Button>
     </Box>
   );
 }
--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,14 +1,97 @@
+import { useContext, useState } from 'react';
+import { UserContext } from '../../contexts/userContext';
+import { createUser, createUserDocument } from '../../utils/firebase';
+
 import { Box, Button, TextField } from '@mui/material';
 
 import { sxStyles } from './SignUp.styles';
 
+const defaultFields = {
+  displayName: '',
+  email: '',
+  password: '',
+  confirmPassword: '',
+};
+
 export default function SignUp() {
+  const [fields, setFields] = useState(defaultFields);
+  const { displayName, email, password, confirmPassword } = fields;
+
+  const { setNotifications, setCurrentUser } = useContext(UserContext);
+
+  const handleSubmit = async () => {
+    if (!displayName || !email || !password || !confirmPassword) {
+      setNotifications({
+        open: true,
+        type: 'error',
+        message: 'Please fill in all fields',
+      });
+    } else if (password !== confirmPassword) {
+      setNotifications({
+        open: true,
+        type: 'error',
+        message: 'Passwords do not match',
+      });
+    } else {
+      try {
+        const { user } = await createUser(email, password);
+
+        setCurrentUser(user);
+        setFields(defaultFields);
+        setNotifications({
+          open: true,
+          type: 'success',
+          message: 'Sign Up Successfull 🎉. Welcome ' + (displayName || email),
+        });
+        await createUserDocument(user, { displayName });
+      } catch (e) {
+        setNotifications({
+          open: true,
+          type: 'error',
+          message: e.message,
+        });
+      }
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFields({ ...fields, [name]: value });
+  };
+
   return (
     <Box sx={sxStyles.container}>
-      <TextField label="Enter Email" type="email" />
-      <TextField label="Enter Password" type="password" />
-      <TextField label="Confirm Password" type="password" />
-      <Button sx={sxStyles.button}>sign up</Button>
+      <TextField
+        onChange={handleChange}
+        label="Name"
+        type="name"
+        name="displayName"
+        value={displayName}
+      />
+      <TextField
+        onChange={handleChange}
+        label="Email"
+        type="email"
+        name="email"
+        value={email}
+      />
+      <TextField
+        onChange={handleChange}
+        label="Password"
+        type="password"
+        name="password"
+        value={password}
+      />
+      <TextField
+        onChange={handleChange}
+        label="Confirm Password"
+        type="password"
+        name="confirmPassword"
+        value={confirmPassword}
+      />
+      <Button onClick={handleSubmit} sx={sxStyles.button}>
+        sign up
+      </Button>
     </Box>
   );
 }
