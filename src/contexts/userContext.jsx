c2f1ede2b7b1a0f83cbb42feef58bc39ab34948f
Vishal Chaurasia <vishalbg.mz@gmail.com>
2022-10-11 17:38:02 +0530

    update userContext

--- src/contexts/userContext.jsx
+++ src/contexts/userContext.jsx
@@ -1,18 +1,38 @@
 import { createContext, useState, useEffect } from 'react';
 import {
+  db,
+  doc,
   createUserDocument,
   onAuthStateChangedListener,
 } from '../utils/firebase';
 
+import { onSnapshot } from 'firebase/firestore';
+
 export const UserContext = createContext({
   currentUser: null,
   setCurrentUser: () => {},
+  notifications: {},
+  setNotifications: () => {},
+  watchlist: [],
+  setWatchlist: () => {},
 });
 
 export const UserProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
+  const [watchlist, setWatchlist] = useState([]);
+  const [notifications, setNotifications] = useState({
+    open: false,
+    message: '',
+    type: 'success',
+  });
 
-  const value = { currentUser, setCurrentUser };
+  const value = {
+    currentUser,
+    setCurrentUser,
+    notifications,
+    setNotifications,
+    watchlist,
+  };
 
   useEffect(() => {
     const unsubscribe = onAuthStateChangedListener((user) => {
@@ -20,10 +40,26 @@ export const UserProvider = ({ children }) => {
         createUserDocument(user);
         setCurrentUser(user);
       }
-
-      return unsubscribe;
     });
+
+    return () => unsubscribe();
   }, []);
+
+  useEffect(() => {
+    if (currentUser) {
+      const coinRef = doc(db, 'watchlist', currentUser.uid);
+
+      const unsubscribe = onSnapshot(coinRef, (coinDoc) => {
+        if (coinDoc.exists()) {
+          setWatchlist(coinDoc.data().coins);
+        } else {
+          console.log('No items in watchlist');
+        }
+      });
+
+      return () => unsubscribe();
+    }
+  }, [currentUser]);
 
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
