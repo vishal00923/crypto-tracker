2a7b519d9fb4dee61dc750f270f4f4b6e6ca4db2
Vishal Chaurasia <vishalbg.mz@gmail.com>
2022-10-12 17:55:26 +0530

    add responsive styles

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -7,28 +7,42 @@ import { sxStyles } from './Banner.styles';
 
 export default function Banner() {
   return (
-    <>
-      <div
-        style={{
-          backgroundImage: `url(./banner.jpg)`,
-          width: '100%',
-          height: '52vh',
-          objectFit: 'cover',
-        }}
-      >
-        <Box sx={sxStyles.box}>
-          <Typography sx={sxStyles.heading} variant="h2" component="h1">
-            Crypto Tracker
-          </Typography>
-          <Typography sx={sxStyles.description} variant="p">
-            Get All The Info Regarding Your Favorite Crypto Currency
-          </Typography>
+    <Box
+      sx={{
+        backgroundImage: `url(./banner.jpg)`,
+        width: '100%',
+        height: {
+          xs: '45vh',
+          sm: '48vh',
+          md: '52vh',
+          lg: '55vh',
+          xl: '58vh',
+        },
+        objectFit: 'cover',
+      }}
+    >
+      <Box sx={sxStyles.box}>
+        <Typography sx={sxStyles.heading} variant="h2" component="h1">
+          Crypto Tracker
+        </Typography>
+        <Typography sx={sxStyles.description} variant="p">
+          Get All The Info Regarding Your Favorite Crypto Currency
+        </Typography>
 
-          <Container sx={{ marginTop: '5%' }}>
-            <Carousal />
-          </Container>
-        </Box>
-      </div>
-    </>
+        <Container
+          sx={{
+            marginTop: {
+              xs: '20%',
+              sm: '20%',
+              md: '15%',
+              lg: '10%',
+              xl: '5%',
+            },
+          }}
+        >
+          <Carousal />
+        </Container>
+      </Box>
+    </Box>
   );
 }
--- src/components/Banner/Banner.styles.js
+++ src/components/Banner/Banner.styles.js
@@ -3,11 +3,24 @@ export const sxStyles = {
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
-    paddingTop: '48px',
+    paddingTop: {
+      xs: '52px',
+      sm: '52px',
+      md: '48px',
+      lg: '32px',
+      xl: '32px',
+    },
   },
 
   heading: {
     color: '#fff',
+    fontSize: {
+      xs: '2.3rem',
+      sm: '2.5rem',
+      md: '2.9rem',
+      lg: '3.2rem',
+      xl: '3.5rem',
+    },
     fontWeight: '900',
     marginBottom: '20px',
     letterSpacing: '2.5px',
